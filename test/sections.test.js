9e8a29b3bd104dcdb1c83075a8cdb5644aa8036e
Benjamin Nortier <bjnortier@Benjamins-MacBook-Pro.local>
2014-07-04 11:26:21 +0100

    basic infra

---
+++ test/sections.test.js
@@ -0,0 +1,11 @@
+var assert = require('chai').assert;
+
+// var dxf = require('../../');
+
+describe('sections', function() {
+
+  it('can parsed from a string', function() {
+    assert.isFalse(true);
+  });
+
+});
