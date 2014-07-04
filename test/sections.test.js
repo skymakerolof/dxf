98cb954a99218bbf4e691ca39b5b266305c08ce2
Ben Nortier <bjnortier@gmail.com>
2014-07-04 14:09:41 +0100

    first steps

--- test/sections.test.js
+++ test/sections.test.js
@@ -1,10 +1,15 @@
+var fs = require('fs');
 var assert = require('chai').assert;
 
-// var dxf = require('../../');
+var dxf = require('..');
 
 describe('sections', function() {
 
+  var floorPlan1 = fs.readFileSync(__dirname + '/../examples/floorplan1.dxf', 'utf-8');
+
   it('can parsed from a string', function() {
+    var result = dxf(floorPlan1);
+    console.log(result);
     assert.isFalse(true);
   });
 
