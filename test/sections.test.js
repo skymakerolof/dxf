b8d933c68928ba08f5db8803e225c33151ec9009
Ben Nortier <bjnortier@gmail.com>
2014-07-07 11:55:49 +0100

    change main api

--- test/sections.test.js
+++ test/sections.test.js
@@ -8,7 +8,7 @@ describe('sections', function() {
   var floorPlan1 = fs.readFileSync(__dirname + '/../examples/floorplan1.dxf', 'utf-8');
 
   it('can parsed from a string', function() {
-    var parser = dxf();
+    var parser = dxf.create();
 
     var polylines = [];
     parser.on('lwpolyline', function(polyline) {
