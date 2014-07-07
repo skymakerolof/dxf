9fb184249f8130d50f49c500ab070bcd9822750d
Ben Nortier <bjnortier@gmail.com>
2014-07-07 11:37:48 +0100

    evented parser

--- test/sections.test.js
+++ test/sections.test.js
@@ -8,9 +8,16 @@ describe('sections', function() {
   var floorPlan1 = fs.readFileSync(__dirname + '/../examples/floorplan1.dxf', 'utf-8');
 
   it('can parsed from a string', function() {
-    var result = dxf(floorPlan1);
-    console.log(result);
-    assert.isFalse(true);
+    var parser = dxf();
+
+    var polylines = [];
+    parser.on('lwpolyline', function(polyline) {
+      polylines.push(polyline);
+    });
+
+    parser.parseString(floorPlan1);
+
+    assert.equal(polylines.length, 124);
   });
 
 });
