7cbd9f53919efff411db337aa53d742577dfd493
Benjamin Nortier <bjnortier@benjamins-mbp.config>
2014-07-08 22:38:22 +0100

    header with min and max

--- test/blocks.js
+++ test/blocks.js
@@ -6,21 +6,16 @@ var floorPlan1 = fs.readFileSync(__dirname + '/../examples/floorplan1.dxf', 'utf
 
 describe('blocks', function() {
 
-
   it('can parsed from a string', function() {
     var parser = dxf.createParser();
 
     var blocks = [];
     parser.on('block', function(block) {
-      if (block.handle === '1F') {
-        console.log(block);
-      }
       blocks.push(block);
     });
 
     parser.parseString(floorPlan1);
 
-    console.log(blocks[0]);
     assert.equal(blocks.length, 147);
   });
 
