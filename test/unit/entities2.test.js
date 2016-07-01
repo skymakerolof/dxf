0317b1e929f19d0e5b69c0ce6346db33d49a3542
Ben Nortier <bjnortier@gmail.com>
2016-07-01 15:58:44 +0200

    wip

--- test/unit/entities2.test.js
+++ test/unit/entities2.test.js
@@ -1,16 +1,28 @@
 'use strict';
 
 const fs = require('fs');
-const assert = require('chai').assert;
+// const assert = require('chai').assert;
 const lib = require('../..');
 
-const dfxContents = fs.readFileSync(__dirname + '/../resources/supportedentities.dxf', 'utf-8');
+const dfxContents = fs.readFileSync(__dirname + '/../resources/blocks2.dxf', 'utf-8');
 
 describe.only('All Entities', () => {
 
   it('can be parsed', () => {
-    const entities = lib.parseString(dfxContents).entities;
-    assert.deepEqual(entities.length, 56);
+    const result = lib.parseString(dfxContents);
+
+    result.blocks.forEach((b) => {
+      if (b.name.indexOf('block') !== -1) {
+        console.log('BLOCK', b);
+      }
+    });
+    
+    result.entities.forEach((e) => {
+      if (e.type === 'INSERT') {
+        console.log('INSERT', e);
+      }
+    });
+
   });
 
 });
