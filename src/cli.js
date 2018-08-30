10fbfbdb8fe239501cb8abf768ff527c7a21d02f
Joge <joge@application-studios.com>
2018-08-30 11:15:05 +0200

    feat(CLI): add dxf-to-svg cli command

---
+++ src/cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import commander from 'commander'
+import fs from 'fs'
+
+import { denormalise, groupEntitiesByLayer, parseString, toSVG } from './'
+
+commander
+  .version(require('../package.json').version)
+  .description('Converts a dxf file to a svg file.')
+  .arguments('<dxfFile> [svgFile]')
+  .option('-v --verbose', 'Verbose output')
+  .action((dxfFile, svgFile, options) => {
+    const parsed = parseString(fs.readFileSync(dxfFile, 'utf-8'))
+
+    if (options.verbose) {
+      console.log('[layer : number of entities]')
+
+      const groups = groupEntitiesByLayer(denormalise(parsed))
+
+      Object.keys(groups).forEach(layer => {
+        console.log(`${layer} : ${groups[layer].length}`)
+      })
+    }
+
+    fs.writeFileSync(svgFile || `${dxfFile.split('.').slice(0, -1).join('.')}.svg`, toSVG(parsed), 'utf-8')
+  })
+  .parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  commander.help()
+}
