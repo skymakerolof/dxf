29f9a6d799f1b71acd470c714df63a3e264be412
Jakob Pallhuber <joge@application-studios.com>
2018-09-04 11:45:02 +0200

    feat(CLI): add dxf-to-svg cli command (#33)
    * fix(GitIgnore): add .idea directory
    * fix(Dependencies): upgrade browserify
    * fix(Dependencies): upgrade chai
    * fix(Dependencies): upgrade lodash pakcages
    * fix(Dependencies): remove unused lodash dependencies
    * fix(Dependencies): upgrade standard
    * chore: fix linting
    * feat(CLI): add dxf-to-svg cli command

--- test/unit/layerstyles.test.js
+++ test/unit/layerstyles.test.js
@@ -10,25 +10,25 @@ describe('Layer Styles', () => {
   it('can be parsed', () => {
     const result = parseString(dxfContents)
     const expected = {
-      '0': {colorNumber: 7},
-      'wall high': {colorNumber: 5},
-      'wall low': {colorNumber: 140},
-      texture: {colorNumber: 253},
-      equipment: {colorNumber: 40},
-      nivel: {colorNumber: 30},
-      doorswindows: {colorNumber: 41},
-      projection: {colorNumber: 134},
-      names: {colorNumber: 7},
-      Defpoints: {colorNumber: 7},
-      topography: {colorNumber: 132},
-      plants: {colorNumber: 83},
+      '0': { colorNumber: 7 },
+      'wall high': { colorNumber: 5 },
+      'wall low': { colorNumber: 140 },
+      texture: { colorNumber: 253 },
+      equipment: { colorNumber: 40 },
+      nivel: { colorNumber: 30 },
+      doorswindows: { colorNumber: 41 },
+      projection: { colorNumber: 134 },
+      names: { colorNumber: 7 },
+      Defpoints: { colorNumber: 7 },
+      topography: { colorNumber: 132 },
+      plants: { colorNumber: 83 },
       'Ceco.NET 53': { colorNumber: 254 }
     }
 
     const reduced = {}
     Object.keys(result.tables.layers).forEach(name => {
       const l = result.tables.layers[name]
-      reduced[name] = {colorNumber: l.colorNumber}
+      reduced[name] = { colorNumber: l.colorNumber }
     })
     assert.deepEqual(reduced, expected)
   })
