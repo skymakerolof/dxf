3a4ab6fed3467cc9d5be2e344b785a7956742171
Eric Mansfield <ericman314@gmail.com>
2019-10-26 00:02:57 -0600

    Implemented bbox calculation, adjusted tests

--- src/toSVG.js
+++ src/toSVG.js
@@ -67,19 +67,8 @@ const ellipseOrArc = (cx, cy, majorX, majorY, axisRatio, startAngle, endAngle, f
   const ry = axisRatio * rx
   const rotationAngle = -Math.atan2(-majorY, majorX)
 
-  const bbox = [
-    { x: rx, y: ry },
-    { x: rx, y: ry },
-    { x: -rx, y: -ry },
-    { x: -rx, y: ry }
-  ].reduce((acc, p) => {
-    const rotated = rotate(p, rotationAngle)
-    acc.expandByPoint({
-      x: cx + rotated.x,
-      y: cy + rotated.y
-    })
-    return acc
-  }, new Box2())
+  const bbox = bboxEllipseOrArc(cx, cy, majorX, majorY, axisRatio, startAngle, endAngle, flipX)
+
   if ((Math.abs(startAngle - endAngle) < 1e-9) || (Math.abs(startAngle - endAngle + Math.PI * 2) < 1e-9)) {
     // Use a native <ellipse> when start and end angles are the same, and
     // arc paths with same start and end points don't render (at least on Safari)
@@ -112,6 +101,73 @@ const ellipseOrArc = (cx, cy, majorX, majorY, axisRatio, startAngle, endAngle, f
     const element = `<path d="${d}" />`
     return { bbox, element }
   }
+}
+
+/**
+ * Compute the bounding box of an elliptical arc, given the DXF entity parameters
+ */
+const bboxEllipseOrArc = (cx, cy, majorX, majorY, axisRatio, startAngle, endAngle, flipX) => {
+  // The bounding box will be defined by the starting point of the ellipse, and ending point,
+  // and any extrema on the ellipse that are between startAngle and endAngle.
+  // The extrema are found by setting either the x or y component of the ellipse's
+  // tangent vector to zero and solving for the angle.
+
+  // Ensure start and end angles are > 0 and well-ordered
+  while (startAngle < 0) startAngle += Math.PI * 2
+  while (endAngle <= startAngle) endAngle += Math.PI * 2
+
+  // When rotated, the extrema of the ellipse will be found at these angles
+  let angles = []
+
+  if (Math.abs(majorX) < 1e-12 || Math.abs(majorY) < 1e-12) {
+    // Special case for majorX or majorY = 0
+    for (let i = 0; i < 4; i++) {
+      angles.push(i / 2 * Math.PI)
+    }
+  } else {
+    // reference https://github.com/bjnortier/dxf/issues/47#issuecomment-545915042
+    angles[0] = Math.atan(-majorY * axisRatio / majorX) - Math.PI // Ensure angles < 0
+    angles[1] = Math.atan(majorX * axisRatio / majorY) - Math.PI
+    angles[2] = angles[0] - Math.PI
+    angles[3] = angles[1] - Math.PI
+  }
+
+  // Remove angles not falling between start and end
+  for (let i = 4; i >= 0; i--) {
+    while (angles[i] < startAngle) angles[i] += Math.PI * 2
+    if (angles[i] > endAngle) {
+      angles.splice(i, 1)
+    }
+  }
+
+  // Also to consider are the starting and ending points:
+  angles.push(startAngle)
+  angles.push(endAngle)
+
+  // Compute points lying on the unit circle at these angles
+  let pts = angles.map(a => ({
+    x: Math.cos(a),
+    y: Math.sin(a)
+  }))
+
+  // Transformation matrix, formed by the major and minor axes
+  let M =
+    [[majorX, -majorY * axisRatio],
+      [majorY, majorX * axisRatio]]
+
+  // Rotate, scale, and translate points
+  let rotatedPts = pts.map(p => ({
+    x: p.x * M[0][0] + p.y * M[0][1] + cx,
+    y: p.x * M[1][0] + p.y * M[1][1] + cy
+  }))
+
+  // Compute extents of bounding box
+  const bbox = rotatedPts.reduce((acc, p) => {
+    acc.expandByPoint(p)
+    return acc
+  }, new Box2())
+
+  return bbox
 }
 
 /**
