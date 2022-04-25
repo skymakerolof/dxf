0ba8236b9394bec9e7b01ac91e825ca6a2cf4d86
Ryan Gaus <rsg1egoman@gmail.com>
2022-04-25 12:41:39 -0400

    feat: add "ATTRIB" entity support
    Behind the scenes, an ATTRIB entity has all the same fields as a MTEXT
    entity.

--- src/handlers/entities.js
+++ src/handlers/entities.js
@@ -14,6 +14,7 @@ import insert from './entity/insert'
 import threeDFace from './entity/threeDFace'
 import dimension from './entity/dimension'
 import text from './entity/text'
+import attrib from './entity/attrib'
 
 const handlers = [
   point,
@@ -28,6 +29,7 @@ const handlers = [
   solid,
   mtext,
   text,
+  attrib,
   insert,
   dimension,
   threeDFace
