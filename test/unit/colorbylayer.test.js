80e9fa119afaf5b3e5f4dcd73583c4a63b0876a8
Rafa <rafag.1858@hotmail.es>
2022-08-29 10:54:11 +0200

    fix: handle color value 256 (#104)
    When an entity has "ByLayer 62" we want to use the color defined in the entity instead of the layer, unless the color is 256. 256 is a special value to say that the entity has to use the color defined in the layer. 
    
    Co-authored-by: Rafa <rafa@debian-BULLSEYE-live-builder-AMD64>

---
+++ test/unit/colorbylayer.test.js
@@ -0,0 +1,18 @@
+import expect from 'expect'
+
+import getRGBForEntity from '../../src/getRGBForEntity'
+
+describe('colors', () => {
+  it('Color defined in the entity but with value 256 means that we have to use the color defined in the layer.', () => {
+    const fakeEntity = {
+      layer: "0",
+      colorNumber: 256
+    };
+    const fakeLayers = {
+      "0": {
+        colorNumber: 1
+      }
+    }
+    expect(getRGBForEntity(fakeLayers, fakeEntity)).toEqual([255, 0, 0])
+  })
+})
