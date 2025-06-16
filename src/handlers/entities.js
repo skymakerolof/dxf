ca1d373d1b02650a19bc72b5155c28ecba4523b9
Yurun <admin@yurunsoft.com>
2025-06-16 10:34:32 +0800

    Fix undefined bug

--- src/handlers/entities.js
+++ src/handlers/entities.js
@@ -56,6 +56,9 @@ export default (tuples) => {
       currentEntityTuples = []
       entityGroups.push(currentEntityTuples)
     }
+    if (currentEntityTuples === undefined) {
+      return
+    }
     currentEntityTuples.push(tuple)
   })
 
