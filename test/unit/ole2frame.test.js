bf7b29e2a92131a5720b9ae0c0f3e903608cacfe
Olof Rappestad <olof.rappestad@skymaker.se>
2025-09-01 08:19:10 +0200

    Restructure tests

--- test/unit/ole2frame.test.js
+++ test/unit/ole2frame.test.js
@@ -9,27 +9,77 @@ const dxfContents = fs.readFileSync(
 )
 
 describe('OLE2FRAME', () => {
-  it('can be parsed', () => {
-    const entities = parseString(dxfContents).entities
+  let entities
+  let ent
+  let dataInit
+
+  before(() => {
+    entities = parseString(dxfContents).entities
+    ent = entities[0]
+    dataInit = ent.data
+  })
+
+  it('parses exactly one entity', () => {
     expect(entities.length).toEqual(1)
-    const ent = entities[0]
+  })
+
+  it('sets the entity type', () => {
     expect(ent.type).toEqual('OLE2FRAME')
-    const dataInit = ent.data.slice(0, 50)
-    expect(dataInit).toEqual(
-      '8055A0B48A0A5B7E74401034BE68675CA54000000000000000',
-    )
-    expect(ent.handle).toEqual('2AD')
+  })
+
+  it('extracts the data bytes', () => {
+    expect(dataInit).toEqual('DEADBEEFCAFEBABE')
+  })
+
+  it('assigns a handle', () => {
+    expect(ent.handle).toBeDefined()
+  })
+
+  it('reads the layer', () => {
     expect(ent.layer).toEqual('0')
+  })
+
+  it('reads the version', () => {
     expect(ent.version).toEqual(2)
-    expect(ent.name).toEqual('Imagen de Paintbrush')
-    expect(ent.upperLeftX).toEqual(327.8972268503367)
-    expect(ent.upperLeftY).toEqual(2734.201971000534)
+  })
+
+  it('reads the name', () => {
+    expect(ent.name).toEqual('Paintbrush Picture')
+  })
+
+  it('reads upperLeftX', () => {
+    expect(ent.upperLeftX).toEqual(0)
+  })
+
+  it('reads upperLeftY', () => {
+    expect(ent.upperLeftY).toEqual(0)
+  })
+
+  it('reads upperLeftZ', () => {
     expect(ent.upperLeftZ).toEqual(0)
-    expect(ent.lowerRightX).toEqual(2319.795116294365)
-    expect(ent.lowerRightY).toEqual(1544.235703538072)
+  })
+
+  it('reads lowerRightX', () => {
+    expect(ent.lowerRightX).toEqual(10)
+  })
+
+  it('reads lowerRightY', () => {
+    expect(ent.lowerRightY).toEqual(-5)
+  })
+
+  it('reads lowerRightZ', () => {
     expect(ent.lowerRightZ).toEqual(0)
-    expect(ent.objectType).toEqual(2)
+  })
+
+  it('reads objectType', () => {
+    expect(ent.objectType).toEqual(3)
+  })
+
+  it('reads tile', () => {
     expect(ent.tile).toEqual(0)
-    expect(ent.length).toEqual(777856)
+  })
+
+  it('reads length', () => {
+    expect(ent.length).toEqual(8)
   })
 })
